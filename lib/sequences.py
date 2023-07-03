3db27a9fafdc9ab8890d23980eebc2f3a2f6f315
Wesley Peters <wesleypeters@gmail.com>
2023-07-03 13:20:50 -0600

    write print_fibonacci() function

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0, 1])
+    else:
+        fibonacci_sequence = [0, 1]
+        while len(fibonacci_sequence) < length:
+            fibonacci_sequence.append(fibonacci_sequence[-1] + fibonacci_sequence[-2])
+        print(fibonacci_sequence)
